8c6cb0dee20914dc2201ebcd3af91ad56c49dc4c
Lawrence Tran <mrlawrencetran@gmail.com>
2019-07-17 04:54:21 +0000

    use file_get_contents to grab the contents of the dummy data json file 'dummy-products_list.json', assign it to a variable called output and print output

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -3,7 +3,7 @@
 function error_handler($error) {
     $output = [
         'success' => 'false',
-        'error' => 'Cannot call to function'
+        'error' => 'Call to undefined function doStuff()'
     ];
     $json_output = json_encode($output);
     print($json_output);
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,13 +1,26 @@
 <?php
-
 include 'functions.php';
 
+require_once('db_connection.php');
+require_once('functions.php');
+
+set_exception_handler('error_handler');
+
+doStuff();
+
+$output = file_get_contents('dummy-products-list.json');
+print($output);
+
+if(!$conn) {
+    print(mysqli_connect_error());
+}
+
 //header('Content-Type: application/json');
-//
+
 //if (empty($_GET['id'])) {
 //  readfile('dummy-products-list.json');
 //} else {
 //  readfile('dummy-product-details.json');
 //}
-
+//
 ?>
