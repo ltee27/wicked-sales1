5a2cb17acd1d3f50ec8d6c5212898fbed4b65c22
Lawrence Tran <mrlawrencetran@gmail.com>
2019-07-17 00:44:08 +0000

    started be f1

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/app';
-import ProductDetails from './components/product-details';
+import App from './components/app';
+// import ProductDetails from './components/product-details';
 
 ReactDOM.render(
-  <ProductDetails/>,
+  <App/>,
   document.querySelector('#root')
 );
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,15 +1,19 @@
 <?php
 include 'functions.php';
 
+require_once('db_connection.php');
 require_once('functions.php');
 
 set_exception_handler('error_handler');
 
-//doStuff();
+doStuff();
 
 $output = file_get_contents('dummy-products-list.json');
 print($output);
 
+if(!$conn) {
+    print(mysqli_connect_error());
+}
 
 //header('Content-Type: application/json');
 
