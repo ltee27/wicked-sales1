f14e8685563156eb14341f915407a0fd7a1c3012
Lawrence Tran <mrlawrencetran@gmail.com>
2019-07-17 17:00:58 +0000

    fixed hard coded error message

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -3,7 +3,7 @@
 function error_handler($error) {
     $output = [
         'success' => 'false',
-        'error' => 'Call to undefined function doStuff()'
+        $error => getMessage()
     ];
     $json_output = json_encode($output);
     print($json_output);
